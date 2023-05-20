43c65b33ad13b2ec42cc3cfcd9f99c80a82cde30
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-05-20 23:48:35 +0530

    chor: update on jest

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
-import { sendVerificationTokenSms, checkVerificationToken, registerUser, loginUser, userVerification } from '../controller/authentication.controller';
-import { body } from 'express-validator';
+import { sendVerificationTokenSms, checkVerificationToken, registerUser, loginUser, userVerification, } from '../controller/authentication.controller';
+
 import SignInmiddleware from '../middleware/signInmiddleware';
 import SignUpmiddleware from '../middleware/signUpmiddleware';
 
@@ -10,8 +10,8 @@ const router = express.Router();
 router.post('/send-verification-token', sendVerificationTokenSms);
 router.post('/check-verification-token', checkVerificationToken);
 router.get('/verifyUser', userVerification);
-router.post('/register',SignUpmiddleware(), registerUser);
-router.post('/login',SignInmiddleware(), loginUser);
+router.post('/register', SignUpmiddleware(), registerUser);
+router.post('/login', SignInmiddleware(), loginUser);
 
 
 export default router;
