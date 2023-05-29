235c5de21367e05016bd59348b36fe67d4cd8615
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-05-29 07:50:20 +0530

    chor: console logs removed

--- src/firebase/serviceAccount.ts
+++ src/firebase/serviceAccount.ts
@@ -14,7 +14,7 @@ async function fetchServiceAccount() {
         const blobContents = await streamToBuffer(downloadResponse.readableStreamBody as NodeJS.ReadableStream);
         
         const serviceAccount : object = JSON.parse(blobContents.toString());
-        console.log(serviceAccount);
+
 
         return serviceAccount;
     } catch (err) {
